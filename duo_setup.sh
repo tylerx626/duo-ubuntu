e3e26c04f450eae4a20d55286d1585291d26d6a9
Tyler <tyler@carlisletech.com>
2025-08-13 15:07:06 -0500

    Update duo_setup.sh
    Remove build and make info

--- duo_setup.sh
+++ duo_setup.sh
@@ -9,41 +9,13 @@ sudo apt-get update
 sudo apt-get install -y build-essential libpam-dev libssl-dev wget
 
 
-#download latest Duo version
-if [ test -f duo_unix-latest.tar.gz ]
-then
-    echo "File duo_unix-latest.tar.gz exists"
-else
-    echo "Downloaded latest duo_unix file"
-    wget https://dl.duosecurity.com/duo_unix-latest.tar.gz
-fi
-
-#check if install directory exists and mkdir if not
-if [ -d "/opt/duo_unix_latest" ] 
-then
-    echo "Directory /opt/duo_unix_latest exists." 
-else
-    echo "Error: Directory /opt/duo_unix_latest/ does not exist."
-    sudo mkdir /opt/duo_unix_latest
-fi
-
-#extract downloaded tarball and change directory 
-sudo tar zxf duo_unix-latest.tar.gz -C /opt/duo_unix_latest
-
-#find the directory path for the latest version after it extracted
-FOUND_PATH=$(find /opt/duo_unix_latest -type d -name "duo_unix-*" 2>/dev/null)
-
-echo "Found directory: $FOUND_PATH"
-cd $FOUND_PATH
-
-#build and install duo_unix with PAM support
-#$FOUND_PATH/configure --with-pam --prefix=/usr && make -C /opt/duo_unix_latest && sudo make -C /opt/duo_unix_latest install
-sudo ./configure --with-pam --prefix=/usr && make && sudo make install
-
 #Create /etc/apt/sources.list.d/duosecurity.list with the following contents:
 echo "deb [trusted=yes arch=amd64] https://pkg.duosecurity.com/Ubuntu noble main" >> /etc/apt/sources.list.d/duosecurity.list deb 
 
+#For Ubuntu 22.04 and later
 curl -s https://duo.com/DUO-GPG-PUBLIC-KEY.asc | sudo gpg --dearmor -o  /etc/apt/trusted.gpg.d/duo.gpg
+
+#for Ubuntu 18.04 and 20.04
 sudo apt-get update && apt-get install -y duo-unix
 
 #edit /etc/duo/pam_duo.conf with ikey, secret key, and hostname
